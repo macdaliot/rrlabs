d08ff919994613bd3cf52cfed2d432020353745e
Andrea Dainese <andrea.dainese@gmail.com>
2016-10-28 08:46:53 +0000

    20161028

--- linux/usr/local/sbin/update_dnsmasq.sh
+++ linux/usr/local/sbin/update_dnsmasq.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# /usr/local/sbin/update_dnsmasq.sh
 
 DNSMASQ_IP=$(egrep -e "^dhcp-range=[0-9a-f:]+, [0-9a-f:]+, 64, " /etc/dnsmasq.conf | cut -d'=' -f2 | cut -d ',' -f1)
-CURRENT_IP=$(ip -f inet6 addr show dev eth1 | grep global | sed 's/^.*inet6 \([0-9a-f:]\+\)\/64.*/\1/g')
+CURRENT_IP=$(ip -f inet6 addr show dev eth1 2> /dev/null | grep global | sed 's/^.*inet6 \([0-9a-f:]\+\)\/64.*/\1/g')
+
+if [ "${CURRENT_IP}" == "" ]; then
+	logger -p daemon.info -t SCRIPT "No valid IPv6 address on eth1"
+	CURRENT_IP="::1"
+fi
 
 if [ "${DNSMASQ_IP}" != "${CURRENT_IP}000" ]; then
 	logger -p daemon.info -t SCRIPT "Updating dnsmasq.conf IPv6 DHCP range"
@@ -12,9 +16,10 @@ if [ "${DNSMASQ_IP}" != "${CURRENT_IP}000" ]; then
 		exit 1
 	fi
 
-	systemctl reload dnsmasq
+	systemctl restart dnsmasq
+	pgrep dnsmasq > /dev/null 2>&1
 	if [ $? -ne 0 ]; then
-		logger -p daemon.error -t SCRIPT "Failed to reload dnsmasq.conf"
+		logger -p daemon.error -t SCRIPT "Failed to restart dnsmasq.conf"
 		exit 1
 	fi
 fi
