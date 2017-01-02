5bb7990e769cc15745fc5dc215d1f1b2919ce9e2
Andrea Dainese <andrea.dainese@gmail.com>
2017-01-02 17:50:28 +0100

    20161220

--- docker/clean.sh
+++ docker/clean.sh
@@ -3,4 +3,4 @@
 DOCKER="docker -H=tcp://127.0.0.1:4243"
 
 ${DOCKER} rm $(${DOCKER}  ps -a -q)
-${DOCKER} rmi $(${DOCKER} images | grep "^<none>" | awk "{print $3}")
+${DOCKER} rmi $(${DOCKER} images | grep "^<none>" | awk '{ print $3 }')
