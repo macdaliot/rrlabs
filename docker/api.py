12529a9dc52a77c696ff11df3c84bc924179da8b
Andrea Dainese <andrea.dainese@gmail.com>
2017-01-18 17:18:41 +0100

    20170118

--- docker/api.py
+++ docker/api.py
@@ -5,19 +5,8 @@ __copyright__ = 'Andrea Dainese <andrea.dainese@gmail.com>'
 __license__ = 'https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode'
 __revision__ = '20170105'
 
-import flask, flask_sqlalchemy, functools
+import flask, flask_sqlalchemy, functools, logging
 from api_modules import *
-
-DEFAULT_API_USERNAME = 'admin'
-DEFAULT_API_PASSWORD = 'eve'
-DB_USERNAME = 'root'
-DB_PASSWORD = 'eve-ng'
-DB_HOST = '127.0.0.1'
-DB_NAME = 'eve'
-
-app = flask.Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{}:{}@{}/{}'.format(DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME)
-db = flask_sqlalchemy.SQLAlchemy(app)
 
 #http://www.vinaysahni.com/best-practices-for-a-pragmatic-restful-api
 #http://flask.pocoo.org/snippets/83/
@@ -42,171 +31,77 @@ db = flask_sqlalchemy.SQLAlchemy(app)
 
 @app.errorhandler(401)
 def http_401(err):
-    output = {}
-    output['code'] = 401
-    output['status'] = 'unauthorized'
-    output['message'] = str(err)
-    return flask.jsonify(output), output['code']
+    response = {
+        'code': 401,
+        'status': 'unauthorized',
+        'message': str(err)
+    }
+    return flask.jsonify(response), response['code']
 
 @app.errorhandler(403)
 def http_403(err):
-    output = {}
-    output['code'] = 401
-    output['status'] = 'forbidden'
-    output['message'] = str(err)
-    return flask.jsonify(output), output['code']
+    response = {
+        'code': 401,
+        'status': 'forbidden',
+        'message': str(err)
+    }
+    return flask.jsonify(response), response['code']
 
 @app.errorhandler(404)
 def http_404(err):
-    output = {}
-    output['code'] = 404
-    output['status'] = 'fail'
-    output['message'] = str(err)
-    return flask.jsonify(output), output['code']
+    response = {
+        'code': 404,
+        'status': 'fail',
+        'message': str(err)
+    }
+    return flask.jsonify(response), response['code']
 
-@app.errorhandler(Exception)
+@app.errorhandler(405)
+def http_405(err):
+    response = {
+        'code': 405,
+        'status': 'fail',
+        'message': str(err)
+    }
+    return flask.jsonify(response), response['code']
+
+@app.errorhandler(409)
+def http_409(err):
+    response = {
+        'code': 409,
+        'status': 'fail',
+        'message': str(err)
+    }
+    return flask.jsonify(response), response['code']
+
+#@app.errorhandler(Exception)
 def http_500(err):
-    output = {}
-    output['code'] = 500
-    output['status'] = 'error'
-    output['message'] = str(err)
-    return flask.jsonify(output), output['code']
+    response = {
+        'code': 500,
+        'status': 'error',
+        'message': str(err)
+    }
+    logging.error(err)
+    return flask.jsonify(response), response['code']
 
-@app.route('/api/users', methods=['GET'])
-@requires_auth
-@requires_roles('admin')
-def getUsers(username = None):
-    output = {}
-    if username == None:
-        output['message'] = 'All users listed'
-        users = User.query
-    else:
-        output['message'] = 'User listed'
-        users = User.query.filter(User.username == username)
-    if users.count() == 0:
-        flask.abort(404)
-    else:
-        output['code'] = 200
-        output['status'] = 'success'
-        output['data'] = {}
-    for user in users:
-        output['data'][user.username] = {
-            'email': user.email,
-            'name': user.name,
-            'username': user.username,
-            'label_start': user.label_start,
-            'label_end': user.label_end
-        }
-    return flask.jsonify(output), output['code']
+# curl -s -D- -u admin:admin -X GET http://127.0.0.1:5000/api/users
+# curl -s -D- -u admin:admin -X POST -d '{"name":"andrea","email":"andrea.dainese@example.com","username":"andrea","password":"andrea"}' -H 'Content-type: application/json' http://127.0.0.1:5000/api/users
+@app.route('/api/users', methods = ['GET', 'POST'])
+@requiresAuth
+@requiresRoles('admin')
+def apiUsers(username = None):
+    if flask.request.method == 'GET':
+        return getUsers()
+    if flask.request.method == 'POST':
+        return addUser()
 
-@app.route('/api/users/<username>', methods=['GET'])
-@requires_auth
-@requires_roles('admin')
-def getUser(username):
+# curl -s -D- -u admin:admin -X GET http://127.0.0.1:5000/api/users/admin
+@app.route('/api/users/<username>', methods = ['GET'])
+@requiresAuth
+@requiresRoles('admin')
+def apiUsersUsername(username):
     return getUsers(username)
-
-
-
-
-
-
-
-
-
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    username = db.Column(db.String(255), primary_key = True)
-    password = db.Column(db.String(255))
-    #roles = db.relationship('Role', secondary='user_roles', backref=db.backref('users', lazy='dynamic'))
-    name = db.Column(db.String(255))
-    email = db.Column(db.String(255), unique = True)
-    label_start = db.Column(db.Integer)
-    label_end = db.Column(db.Integer)
-    def __repr__(self):
-        return '<User({})>'.format(self.username)
-
-#class Role(db.Model):
-#    __tablename__ = 'roles'
-#
-#    id = db.Column(db.Integer(), primary_key = True)
-#    name = db.Column(db.String(50), unique = True)
-#
-#    def __repr__(self):
-#        return '<Role({})>'.format(self.name)
-
-#class UserRoles(db.Model):
-
-
-class Lab(db.Model):
-    __tablename__ = 'labs'
-    id = db.Column(db.String(255), primary_key = True)
-    name = db.Column(db.String(255))
-    path = db.Column(db.String(255))
-    def __repr__(self):
-        return '<Lab(id={})>'.format(id)
-
-class ActiveNode(db.Model):
-    __tablename__ = 'active_nodes'
-    user_id = db.Column(db.String, db.ForeignKey('users.username'))
-    lab_id = db.Column(db.String, db.ForeignKey('labs.id'))
-    node_id = db.Column(db.Integer)
-    iface_id = db.Column(db.Integer)
-    label = db.Column(db.Integer, primary_key = True)
-    state = db.Column(db.String(255))
-    controller = db.Column(db.String(255))
-    def __repr__(self):
-        return '<Node(label={})>'.format(label)
-
-class ActiveTopology(db.Model):
-    __tablename__ = 'active_topologies'
-
-    src = db.Column(db.Integer, db.ForeignKey('active_nodes.label'), primary_key = True)
-    dst = db.Column(db.Integer, db.ForeignKey('active_nodes.label'), primary_key = True)
-
-    def __repr__(self):
-        return '<Topology(src={}, dst={}>'.format(src, dst)
-
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000, debug = True)
-    # http://flask-sqlalchemy.pocoo.org/2.1/quickstart/#a-minimal-application
-    # from yourapplication import db
-    # from yourapplication import User
-
-    #db.create_all()
-    #admin = User(username = 'admin', password = 'admin', name = 'Administrator', email = 'admininistrator@example.com', label_start = 0, label_end = 199)
-    #andrea = User(username = 'andrea', password = 'andrea', name = 'Andrea Dainese', email = 'andrea.dainese@gmail.com', label_start = 200, label_end = 399)
-    #users = [ ]
-    #users.append(admin)
-    #users.append(andrea)
-    
-    #for user in User.query.filter(User.username == 'admin'):
-    #    print("CIAO")
-    #    print(user.name)
-
-    #print(db.session.query(User.id == 'admin'))
-    #if admin not in User.query(User.id == 'admin'):
-    #    print("adding admin")
-    #    users.append(admin)
-    #if andrea not in User.query.all():
-    #    users.append(andrea)
-
-    #for user in users:
-    #    db.session.add(user)
-    #db.session.commit()
-
-    #print(User.query.all())
-    #for user in User.query.all():
-    #    print(user.username)
-
-
-
-#import MySQLdb as mdb
-#con = mdb.connect(self.db_host, self.db_user, self.db_password, self.db)
-#con.set_character_set('utf8')
-#
-#from locale import setlocale
-#setlocale(locale.LC_ALL, "nb_NO.utf8")
 
--- docker/api_modules.py
+++ docker/api_modules.py
@@ -5,37 +5,148 @@ __copyright__ = 'Andrea Dainese <andrea.dainese@gmail.com>'
 __license__ = 'https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode'
 __revision__ = '20170105'
 
-def check_auth(username, password):
-    if username == 'admin' and password == 'admin':
-        return True
-    else:
-        return False
+DEFAULT_API_USERNAME = 'admin'
+DEFAULT_API_PASSWORD = 'eve'
+DB_USERNAME = 'root'
+DB_PASSWORD = 'eve-ng'
+DB_HOST = '127.0.0.1'
+DB_NAME = 'eve'
 
-def check_authz(username, roles):
+import flask, flask_sqlalchemy, functools, hashlib, logging
+
+app = flask.Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{}:{}@{}/{}'.format(DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME)
+db = flask_sqlalchemy.SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    username = db.Column(db.String(255), primary_key = True)
+    password = db.Column(db.String(255))
+    #roles = db.relationship('Role', secondary='user_roles', backref=db.backref('users', lazy='dynamic'))
+    name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique = True)
+    label_start = db.Column(db.Integer)
+    label_end = db.Column(db.Integer)
+    def __repr__(self):
+        return '<User({})>'.format(self.username)
+
+#class Role(db.Model):
+#    __tablename__ = 'roles'
+#
+#    id = db.Column(db.Integer(), primary_key = True)
+#    name = db.Column(db.String(50), unique = True)
+#
+#    def __repr__(self):
+#        return '<Role({})>'.format(self.name)
+
+#class UserRoles(db.Model):
+
+
+class Lab(db.Model):
+    __tablename__ = 'labs'
+    id = db.Column(db.String(255), primary_key = True)
+    name = db.Column(db.String(255))
+    path = db.Column(db.String(255))
+    def __repr__(self):
+        return '<Lab(id={})>'.format(id)
+
+class ActiveNode(db.Model):
+    __tablename__ = 'active_nodes'
+    user_id = db.Column(db.String, db.ForeignKey('users.username'))
+    lab_id = db.Column(db.String, db.ForeignKey('labs.id'))
+    node_id = db.Column(db.Integer)
+    iface_id = db.Column(db.Integer)
+    label = db.Column(db.Integer, primary_key = True)
+    state = db.Column(db.String(255))
+    controller = db.Column(db.String(255))
+    def __repr__(self):
+        return '<Node(label={})>'.format(label)
+
+class ActiveTopology(db.Model):
+    __tablename__ = 'active_topologies'
+
+    src = db.Column(db.Integer, db.ForeignKey('active_nodes.label'), primary_key = True)
+    dst = db.Column(db.Integer, db.ForeignKey('active_nodes.label'), primary_key = True)
+
+    def __repr__(self):
+        return '<Topology(src={}, dst={}>'.format(src, dst)
+
+def checkAuth(username, password):
+    users = User.query.filter(User.username == username)
+    if users.count() != 1:
+        return False
+    if users.one().password != hashlib.sha256(password.encode('utf-8')).hexdigest():
+        return False
+    return True
+
+def checkAuthz(username, roles):
     if username == 'admin':
         return True
     return False
 
-def requires_auth(f):
+def addUser():
+    import json
+    data = json.loads(flask.request.data.decode('utf-8'))
+    user = User(**data)
+    user.password = hashlib.sha256(user.password.encode('utf-8')).hexdigest():
+    users = User.query.filter(User.username == user.username)
+    if users.count() != 0:
+        flask.abort(409)
+    db.session.add(user)
+    db.session.commit()
+    response = {
+        'code': 201,
+        'status': 'success',
+        'message': 'User added'
+    }
+    return flask.jsonify(response), response['code']
+
+def getUser(username):
+    return getUsers(username)
+
+def getUsers(username = None):
+    response = {}
+    if username == None:
+        response['message'] = 'All users listed'
+        users = User.query
+    else:
+        response['message'] = 'User listed'
+        users = User.query.filter(User.username == username)
+    if users.count() == 0:
+        flask.abort(404)
+    else:
+        response['code'] = 200
+        response['status'] = 'success'
+        response['data'] = {}
+    for user in users:
+        response['data'][user.username] = {
+            'email': user.email,
+            'name': user.name,
+            'username': user.username,
+            'label_start': user.label_start,
+            'label_end': user.label_end
+        }
+    return flask.jsonify(response), response['code']
+
+
+def requiresAuth(f):
     import flask, functools
     @functools.wraps(f)
     def decorated(*args, **kwargs):
         auth = flask.request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
+        if not auth or not checkAuth(auth.username, auth.password):
             flask.abort(401)
         return f(*args, **kwargs)
     return decorated
 
-def requires_roles(*roles):
+def requiresRoles(*roles):
     import flask, functools
     def wrapper(f):
         @functools.wraps(f)
         def wrapped(*args, **kwargs):
             auth = flask.request.authorization
-            print(auth)
-            if not check_authz(auth.username, roles):
+            if not checkAuthz(auth.username, roles):
                 flask.abort(403)
             return f(*args, **kwargs)
         return wrapped
     return wrapper
-
