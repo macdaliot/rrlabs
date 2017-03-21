dbb415ceb71c6c0d7548a6d518ecedd9ba2b0819
Andrea Dainese <andrea.dainese@gmail.com>
2017-03-21 19:05:20 +0100

    20170321

--- docker/unetlab/controller/bootstrap.sh
+++ docker/unetlab/controller/bootstrap.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 shutdown() {
-	echo "Shutting down..."
-	ps -ef
+	echo -n "Shutting down... "
 	killall -s SIGTERM mysqld memcached &> /data/logs/shutdown.log
 }
 
-trap shutdown SIGTERM SIGHUP
+echo -n "Starting controller... "
+
+trap shutdown SIGTERM SIGHUP &> /dev/null
 
 if [ ! -d /data/logs ]; then
 	mkdir /data/logs || exit 1
@@ -14,30 +15,23 @@ fi
 
 # Starting MariaDB
 if [ ! -d /data/database/mysql ]; then
-	/usr/bin/mysql_install_db --datadir="/data/database" --user="mysql" &> /data/logs/mysql_install_db.log
+	/usr/bin/mysql_install_db --datadir="/data/database" --user="mysql" &> /data/logs/mariadb_install.log
 	if [ $? -ne 0 ]; then
 		echo "ERROR: failed to initialize MariaDB"
 		cat /data/logs/mysql_install_db.log
 		exit 1
 	fi
 fi
-/usr/bin/mysqld_safe --datadir="/data/database" --nowatch &> /data/logs/mysqld_safe.log
-if [ $? -ne 0 ]; then
-	echo "ERROR: failed to start MariaDB"
-	cat /data/logs/mysqld_safe.log
-	exit 1
-fi
+mkdir -p /run/mysqld &> /dev/null
+/usr/bin/mysqld --basedir=/usr --datadir=/data/database --plugin-dir=/usr/lib/mysql/plugin --user=mysql --pid-file=/run/mysqld/mariadb.pid --socket=/run/mysqld/mysqld.sock --port=3306 &> /data/logs/mariadb.log &
+MARIADB_PID=$!
 
 # Starting Memcached
-/usr/bin/memcached -m 64 -p 11211 -u memcached -l 127.0.0.1 &> /data/logs/memcached.log
-if [ $? -ne 0 ]; then
-	echo "ERROR: failed to start Memcached"
-	cat /data/logs/memcached.log
-	exit 1
-fi
+/usr/bin/memcached -m 64 -p 11211 -u memcached -l 127.0.0.1 &> /data/logs/memcached.log &
+MEMCACHED_PID=$!
 
-echo "Controller started"
+echo "done"
 
-while true; do
-	sleep 10000
-done
+wait $MARIADB_PID $MEMCACHED_PID
+
+echo "done"
--- docker/unetlab/controller/controller.dockerfile
+++ docker/unetlab/controller/controller.dockerfile
@@ -10,14 +10,13 @@ LABEL build = "docker build -t dainok/controller:latest -f controller.dockerfile
 LABEL usage.0 = "docker volume create --name controller_data"
 LABEL usage.1 = "docker run -ti --rm --name controller --volume controller_data:/data dainok/controller /sbin/bootstrap.sh"
 
-#ENTRYPOINT /sbin/bootstrap.sh
+#ENTRYPOINT ["/sbin/bootstrap.sh"]
 EXPOSE :80 :443 :5005
 
 # Installing dependencies
 RUN apk update || exit 1
 RUN apk upgrade || exit 1
-RUN apk add bash mariadb memcached nginx openssh python3 || exit 1
-#RUN apk add mariadb-client || exit 1
+RUN apk add bash mariadb mariadb-client memcached nginx openssh python3 || exit 1
 RUN pip3 install --no-cache-dir --upgrade pip || exit 1
 RUN pip3 install --no-cache-dir Flask-SQLAlchemy python3-memcached || exit 1
 
@@ -30,4 +29,3 @@ COPY bootstrap.sh /sbin/bootstrap.sh
 
 # Cleaning
 RUN find /var/cache/apk/ -type f -delete || exit 1
-
