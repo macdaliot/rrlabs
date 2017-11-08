503e7c6b1066d7d9a7e5cc72805b14c844bd28cd
Andrea Dainese <andrea.dainese@gmail.com>
2017-11-08 15:25:02 +0100

    20171106

--- labs/UNetLabv2 Demo/templates/99line.js
+++ labs/UNetLabv2 Demo/templates/99line.js
@@ -1,22 +1,11 @@
-!
 ip forward-protocol nd
-!
 !
 no ip http server
 no ip http secure-server
 !
 logging host 172.16.0.1
 !
-!
-!
 control-plane
-!
-!
-!
-!
-!
-!
-!
 !
 line con 0
  logging synchronous
@@ -24,6 +13,5 @@ line aux 0
 line vty 0 4
  login local
  transport input ssh
-!
 !
 end
