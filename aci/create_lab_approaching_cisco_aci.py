bb274b429f4fd13fefd027731a7611ba2df6e1ee
Andrea Dainese <andrea.dainese@gmail.com>
2018-04-20 15:33:14 +0200

    20180420

--- aci/create_lab_approaching_cisco_aci.py
+++ aci/create_lab_approaching_cisco_aci.py
@@ -31,37 +31,79 @@ bds = {
 # Reading options
 username, password, apic_host = checkOpts()
 
+# Create users
+users = []
+user_list_ids = [155]
+user_list_ids.extend(range(1, total_users + 1))
+for x in user_list_ids:
+    lab_username = '{}{}'.format(user_prefix, x)
+    lab_password = ''.join(random.choices(string.ascii_uppercase, k = 3) + random.choices(string.ascii_lowercase, k = 3) + random.choices(string.digits, k = 2))
+    users.append({
+        'id': x,
+        'username': lab_username,
+        'password': lab_password
+    })
+print(users)
+
 # Configure physical devices
 driver_ios = napalm.get_network_driver('ios')
+
+routers = {
+    'Router1': {
+        'device': driver_ios(hostname = '172.25.82.12', username = napalm_usernamne, password = napalm_password, optional_args = {'port': 22}),
+        'ip': 12
+    },
+    'Router2': {
+        'device': driver_ios(hostname = '172.25.82.13', username = napalm_usernamne, password = napalm_password, optional_args = {'port': 22}),
+        'ip': 13
+    }
+}
+for router_name, router in routers.items():
+    router_config = 'ip routing\nip domain-name lab.local\nip route 0.0.0.0 0.0.0.0 172.25.82.254\n'
+    for user in users:
+        router_config += 'username {} privilege 1 password {}\n'.format(user['username'], user['password'])
+    if router_name == 'Router1':
+        router_config += 'ip name-server 8.8.8.8 8.8.4.4\nntp server 0.it.pool.ntp.org\nntp server 1.it.pool.ntp.org\nntp server 2.it.pool.ntp.org\nntp server 3.it.pool.ntp.org\n'
+    elif router_name == 'Router2':
+        router_config += 'ip name-server 172.25.82.12\nntp server 172.25.82.12\n'
+        router_config += 'interface GigabitEthernet0/1\nno shutdown\n'
+        router_config += 'ip http server\nno ip http secure-server\nip http authentication local\n'
+        for user in users:
+            router_config += 'vrf definition vrf{}-dmz\naddress-family ipv4\nvrf definition vrf{}-client\naddress-family ipv4\n'.format(user['id'], user['id'])
+            router_config += 'ip route vrf vrf{}-dmz 0.0.0.0 0.0.0.0 10.{}.1.254\n'.format(user['id'], user['id'])
+            router_config += 'interface GigabitEthernet0/1.{}\nencapsulation dot1q {}\nvrf forwarding vrf{}-dmz\nDescription DMZ\nip address 10.{}.1.{} 255.255.255.0\n'.format(2000 + user['id'], 2000 + user['id'], user['id'], user['id'], router['ip'])
+            router_config += 'interface GigabitEthernet0/1.{}\nencapsulation dot1q {}\nvrf forwarding vrf{}-client\nDescription Client\nip address 10.{}.0.{} 255.255.255.0\n'.format(1000 + user['id'], 1000 + user['id'], user['id'], user['id'], router['ip'])
+        router['device'].open()
+        router['device'].load_merge_candidate(config = router_config)
+        router['device'].commit_config()
+        router['device'].close()
+
 switches = {
     'Switch1': {
         'device': driver_ios(hostname = '172.25.82.10', username = napalm_usernamne, password = napalm_password, optional_args = {'port': 22}),
-        'ip': 252
+        'ip': 10
     },
     'Switch2': {
         'device': driver_ios(hostname = '172.25.82.11', username = napalm_usernamne, password = napalm_password, optional_args = {'port': 22}),
-        'ip': 253
+        'ip': 11
     }
 }
 for switch_name, switch in switches.items():
-    switch_config = 'vtp mode transparent\nip domain-name lab.local\nip name-server 172.25.82.12\nntp server 172.25.82.12\n'
+    switch_config = 'vtp mode transparent\nip routing\nip domain-name lab.local\nip name-server 172.25.82.12\nntp server 172.25.82.12\nip route 0.0.0.0 0.0.0.0 172.25.82.254\n'
+    for user in users:
+        switch_config += 'username {} privilege 1 password {}\n'.format(user['username'], user['password'])
     if switch_name == 'Switch1':
-        switch_config = switch_config + 'spanning-tree vlan 100-255 priority 4096\n'
-    else:
-        switch_config = switch_config + 'spanning-tree vlan 100-255 priority 8192\n'
-    for x in range(1, total_users + 1):
-        switch_config = switch_config + 'vlan {}\ninterface vlan{}\nip address 10.{}.0.{} 255.255.255.0\nno shutdown\n'.format(100 + x, 100 + x, x, switch['ip'])
-    switch_config = switch_config + 'vlan 255\ninterface vlan255\nip address 10.255.0.{} 255.255.255.0\nno shutdown\n'.format(switch['ip'])
+        switch_config += 'spanning-tree vlan 100-255 priority 4096\n'
+        switch_config += 'interface range GigabitEthernet1/0/1 - 2\nchannel-group 1 mode active\ninterface range GigabitEthernet1/0/3 - 4\nchannel-group 2 mode active\ninterface range Port-channel1 - 2\nswitchport trunk encapsulation dot1q\nswitchport mode trunk\nswitchport nonegotiate\n'
+    if switch_name == 'Switch2':
+        switch_config += 'spanning-tree vlan 100-255 priority 8192\n'
+        switch_config += 'interface range GigabitEthernet1/0/2, GigabitEthernet1/0/4\nchannel-group 1 mode active\ninterface Port-channel1\nswitchport trunk encapsulation dot1q\nswitchport mode trunk\nswitchport nonegotiate\ninterface GigabitEthernet1/0/1\nswitchport trunk encapsulation dot1q\nswitchport mode trunk\nswitchport nonegotiate\ninterface FastEthernet1/0/1\nswitchport trunk encapsulation dot1q\nswitchport mode trunk\nswitchport nonegotiate\nspanning-tree portfast trunk\n'
+    for user in users:
+        switch_config += 'vlan {}\ninterface vlan{}\nip address 10.{}.0.{} 255.255.255.0\nno shutdown\n'.format(100 + user['id'], 100 + user['id'], user['id'], switch['ip'])
     switch['device'].open()
     switch['device'].load_merge_candidate(config = switch_config)
     switch['device'].commit_config()
     switch['device'].close()
-sys.exit(0)
-
-routers = {
-    'Router1': driver_ios(hostname = '172.25.82.12', username = napalm_usernamne, password = napalm_password, optional_args = {'port': 22}),
-    'Router2': driver_ios(hostname = '172.25.82.13', username = napalm_usernamne, password = napalm_password, optional_args = {'port': 22})
-}
 sys.exit(0)
 
 # Logging in
@@ -107,20 +149,15 @@ for bd_name, bd_description in bds.items():
         logging.error(response)
         sys.exit(255)
 
-# Create users
-users = {}
-for x in range(1, total_users + 1):
-    lab_username = '{}{}'.format(user_prefix, x)
-    lab_password = ''.join(random.choices(string.ascii_uppercase, k = 3) + random.choices(string.ascii_lowercase, k = 3) + random.choices(string.digits, k = 2))
-    #response_code, response = addUser(apic_host = apic_host, token = token, cookies = cookies, username = lab_username, password = lab_password)
-    #if response_code == 200:
-    #    logging.info('added user "{}" with password "{}"'.format(lab_username, lab_password))
-    #    users[lab_username] = {
-    #        'password': lab_password
-    #    }
-    #else:
-    #    logging.error('failed to add user "{}" with password "{}"'.format(lab_username, lab_password))
-    #    logging.error(response)
-    #    sys.exit(255)
-
-print(users)
+# Add users
+for lab_username, lab_password in users.items():
+    response_code, response = addUser(apic_host = apic_host, token = token, cookies = cookies, username = lab_username, password = lab_password)
+    if response_code == 200:
+        logging.info('added user "{}" with password "{}"'.format(lab_username, lab_password))
+        users[lab_username] = {
+            'password': lab_password
+        }
+    else:
+        logging.error('failed to add user "{}" with password "{}"'.format(lab_username, lab_password))
+        logging.error(response)
+        sys.exit(255)
