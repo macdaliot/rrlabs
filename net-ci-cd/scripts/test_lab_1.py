26954b14ebfc9849047455afdf46a230277b051a
Andrea Dainese <andrea.dainese@gmail.com>
2018-08-24 09:44:58 +0200

    Added Ansible playbook for EVE-NG

--- net-ci-cd/scripts/test_lab_1.py
+++ net-ci-cd/scripts/test_lab_1.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python3
 from nornir.core import InitNornir
 from nornir.plugins.functions.text import print_result
-from nornir.plugins.tasks.networking import napalm_get
+from nornir.plugins.tasks.networking import netmiko_send_command
 
-import logging, sys
+import sys
 
 def main():
     testing_ok = True
     nr = InitNornir(host_file = 'hosts.yaml', group_file = 'groups.yaml', num_workers = 20)
     test = nr.filter(site = 'test')
-    result = test.run(task = napalm_get, getters = ['get_lldp_neighbors'])
 
-    # Checking LLDP neighbors
+    # Checking CDP neighbors
+    command = 'show cdp neighbors detail'
+    result = test.run(task = netmiko_send_command, command_string = command, use_textfsm = True)
     for device_name, device_output in result.items():
         if device_name == 'R1-test':
-            if device_output[0].result['get_lldp_neighbors']['Ethernet0/0'][0]['hostname'] != 'R2.test.example.com':
-                logging.error('failed to get right neighbors on {}'.format(device_name))
+            if device_output.result[0]['destination_host'] != 'R2.test.example.com':
+                print('failed to get right neighbors on {}'.format(device_name))
                 testing_ok = False
         if device_name == 'R2-test':
-            if device_output[0].result['get_lldp_neighbors']['Ethernet0/0'][0]['hostname'] != 'R1.test.example.com':
-                logging.error('failed to get right neighbors on {}'.format(device_name))
+            if device_output.result[0]['destination_host'] != 'R1.test.example.com':
+                print('failed to get right neighbors on {}'.format(device_name))
+                testing_ok = False
+
+    # Checking OSPF neighbors
+    command = 'show ip ospf neighbor'
+    result = test.run(task = netmiko_send_command, command_string = command, use_textfsm = True)
+    for device_name, device_output in result.items():
+        if device_name == 'R1-test':
+            if device_output.result[0]['address'] != '192.168.0.2':
+                print('failed to get the right OSPF neighbor on {}'.format(device_name))
+                testing_ok = False
+        if device_name == 'R2-test':
+            if device_output.result[0]['address'] != '192.168.0.1':
+                print('failed to get the right OSPF neighbor on {}'.format(device_name))
                 testing_ok = False
 
     if not testing_ok:
